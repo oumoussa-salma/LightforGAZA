d4768e0a8994ba91d8ac7f3be65737cb399a43fc
souhailELMAHDANI <elmahdanisouhail@gmail.com>
2025-04-12 18:15:52 +0100

    Commit to test

--- Conflicting.tsx
+++ Conflicting.tsx
@@ -5,6 +5,7 @@ const Conflicting: React.FC = () => {
         <div>
             <h1>Welcome to Light for GAZA</h1>
             <p>This is the Conflicting component.</p>
+            <p>By Souhail</p>
         </div>
     );
 };
